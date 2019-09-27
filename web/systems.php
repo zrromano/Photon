28fdb0211e6a082f73cd0ac171833f25c939651d
Ethan Ruiz <45ruiz@gmail.com>
2019-09-27 13:05:04 -0700

    databse struff

--- web/systems.php
+++ web/systems.php
@@ -1,7 +1,7 @@
 <?php
 $params = parse_ini_file('home/atomvm/Documents/CMU/loudcloud/database.ini');
 if ($params === false) {
-  throw new \Exception("Error reading database configuration file");
+  throw new \Exception("Error reading database config file");
 }
 // connect to the postgresql database
 $dbhost=$params['host'];
@@ -15,3 +15,17 @@ $conStr = sprintf("pgsql:host=%s;port=%d;dbname=%s;user=%s;password=%s",
                 $dbname,
                 $dbuser,
                 $dbpassword);
+
+try{
+  $pdo = new \PDO($conStr);
+  $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+  echo "connection succesful"
+  $sql = "SELECT * FROM public.userinfo"
+  $statement = $pdo->query($sql);
+  while ($row = $statement->fetch()){
+    echo $row['username'] . $row['password'] . " <br>";}
+  }
+  catch(PDOException $e){
+    echo "connection failed <br>" . $e->getMessage();
+  }
+}
