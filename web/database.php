51e7b92c3af22c88bb6211523f712ea0c1427a30
Ethan Ruiz <45ruiz@gmail.com>
2019-10-02 13:49:09 -0700

    get off my damn branch zach

--- web/database.php
+++ web/database.php
@@ -21,20 +21,11 @@ try{
   $pdo = new \PDO($conStr);
   $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
   echo "connection succesful";
-<<<<<<< HEAD
   $sql = "SELECT * FROM public.userinfo";
-=======
-  $sql = "SELECT * FROM logins";
->>>>>>> d48414a6f74cbd8526fe6d5148ba0a64fe288d4a
   $statement = $pdo->query($sql);
   while ($row = $statement->fetch()){
     echo $row['username'] . $row['password'] . " <br>";}
   }
 catch(PDOException $e){
-<<<<<<< HEAD
-    echo "connection failed <br>" . $e->getMessage();
-  }
-=======
   echo "connection failed <br>" . $e->getMessage();
 }
->>>>>>> d48414a6f74cbd8526fe6d5148ba0a64fe288d4a
