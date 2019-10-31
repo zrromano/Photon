abed52ac0165094069aff4d90cf28f8aa3eb415d
zrromano <crazyscarecrow2@hotmail.com>
2019-10-31 10:24:53 -0700

    extract movies table

--- vidly/src/components/Movies.jsx
+++ vidly/src/components/Movies.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import Like from "./common/like";
 import Pagination from "./common/pagination";
 import ListGroup from "./common/listGroup";
+import MoviesTable from "./moviesTable";
 import { getMovies } from "../services/fakeMovieService";
 import { getGenres } from "../services/fakeGenreService";
 import { paginate } from "../utilities/paginate";
@@ -15,7 +15,11 @@ class Movies extends Component {
   };
 
   componentDidMount() {
-    this.setState({ movies: getMovies(), genres: getGenres() });
+    const genres = [{ name: "All Genres" }, ...getGenres()];
+    this.setState({
+      movies: getMovies(),
+      genres
+    });
   }
 
   handleDelete = movie => {
@@ -36,64 +40,41 @@ class Movies extends Component {
   };
 
   handleGenreSelect = genre => {
-    console.log(genre);
+    this.setState({ currentPage: 1, selectedGenre: genre });
   };
 
   render() {
     const { length: count } = this.state.movies;
-    const { pageSize, currentPage, movies } = this.state;
+    const { pageSize, currentPage, selectedGenre, movies } = this.state;
 
     if (count === 0) return <p>There are no movies in the database.</p>;
-
-    const paginatedMovies = paginate(movies, currentPage, pageSize);
+    const filtered =
+      selectedGenre && selectedGenre._id
+        ? movies.filter(m => m.genre._id === selectedGenre._id)
+        : movies;
+    const paginatedMovies = paginate(filtered, currentPage, pageSize);
     return (
       <div className="row">
         <div className="col-2">
           <ListGroup
             items={this.state.genres}
+            selectedItem={
+              this.state.selectedGenre
+                ? this.state.selectedGenre
+                : this.state.genres[0]
+            }
             onItemSelect={this.handleGenreSelect}
           />
         </div>
         <div className="col">
-          <p>Showing {count} movies in the database.</p>
-          <table className="table">
-            <thead>
-              <tr>
-                <th scope="col">Title</th>
-                <th scope="col">Genre</th>
-                <th scope="col">Stock</th>
-                <th scope="col">Rate</th>
-                <th scope="col" />
-                <th scope="col" />
-              </tr>
-            </thead>
-            <tbody>
-              {paginatedMovies.map(movie => (
-                <tr key={movie._id}>
-                  <td>{movie.title}</td>
-                  <td>{movie.genre.name}</td>
-                  <td>{movie.numberInStock}</td>
-                  <td>{movie.dailyRentalRate}</td>
-                  <td>
-                    <Like
-                      liked={movie.liked}
-                      onClick={() => this.handleLike(movie)}
-                    />
-                  </td>
-                  <td>
-                    <button
-                      onClick={() => this.handleDelete(movie)}
-                      className="btn btn-danger btn-sm"
-                    >
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          <p>Showing {filtered.length} movies in the database.</p>
+          <MoviesTable
+            movies={movies}
+            onLike={this.handleLike}
+            onDelete={this.onDelete}
+          />
           <Pagination
-            itemsCount={count}
+            itemsCount={filtered.length}
             pageSize={pageSize}
             currentPage={currentPage}
             onPageChange={this.handlePageChange}
--- vidly/src/components/common/listGroup.jsx
+++ vidly/src/components/common/listGroup.jsx
@@ -1,11 +1,35 @@
 import React from "react";
 
-const ListGroup = () => {
+const ListGroup = props => {
+  const {
+    items,
+    textProperty,
+    valueProperty,
+    selectedItem,
+    onItemSelect
+  } = props;
+
   return (
     <ul className="list-group">
-      <li className="list-group-item">All Genres</li>
+      {items.map(item => (
+        <li
+          onClick={() => onItemSelect(item)}
+          key={item[valueProperty] ? item[valueProperty] : 0}
+          className={
+            item === selectedItem ? "list-group-item active" : "list-group-item"
+          }
+          style={{ cursor: "pointer" }}
+        >
+          {item[textProperty]}
+        </li>
+      ))}
     </ul>
   );
+};
+
+ListGroup.defaultProps = {
+  textProperty: "name",
+  valueProperty: "_id"
 };
 
 export default ListGroup;
