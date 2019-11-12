72819c5c59b2226cdb5303cb36acda28e9a3c21f
zrromano <crazyscarecrow2@hotmail.com>
2019-11-11 16:05:20 -0800

    user registration and authentication

--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -2,32 +2,43 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Joi from "joi-browser";
 import Form from "./common/form";
+import { login } from "../services/authService";
 
 class LoginForm extends Form {
   state = {
-    data: { username: "", password: "" },
+    data: { email: "", password: "" },
     errors: {}
   };
 
   schema = {
-    username: Joi.string()
+    email: Joi.string()
       .required()
-      .label("Username"),
+      .label("E-mail Address"),
     password: Joi.string()
       .required()
       .label("Password")
   };
 
-  doSubmit = () => {
-    //Call the server
-    console.log("Submitted");
+  doSubmit = async () => {
+    try {
+      const { data } = this.state;
+      const { data: jwt } = await login(data.username, data.password);
+      localStorage.setItem("token", jwt);
+      this.props.history.push("/");
+    } catch (ex) {
+      if (ex.response && ex.response.status === 400) {
+        const errors = { ...this.state.errros };
+        errors.username = ex.response.data;
+        this.setState({ errors });
+      }
+    }
   };
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <h1>Login</h1>
-        {this.renderInput("username", "Username")}
+        {this.renderInput("email", "E-mail Address")}
         {this.renderInput("password", "Password", "password")}
 
         {this.renderButton("Login")}
--- vidly/src/components/registerForm.jsx
+++ vidly/src/components/registerForm.jsx
@@ -1,38 +1,58 @@
 import React, { Component } from "react";
 import Joi from "joi-browser";
 import Form from "./common/form";
+import * as userService from "../services/userService";
 
 class RegisterForm extends Form {
   state = {
-    data: { fullName: "", username: "", password: "" },
+    data: { name: "", email: "", password: "" },
     errors: {}
   };
 
   schema = {
-    fullName: Joi.string()
+    name: Joi.string()
       .required()
       .label("Full Name"),
-    username: Joi.string()
-      .alphanum()
-      .min(3)
+    email: Joi.string()
+      .min(5)
       .max(30)
       .required()
-      .label("Username"),
+      .label("Email"),
     password: Joi.string()
       .min(3)
       .required()
       .label("Password")
   };
 
-  doSubmit = () => {};
+  doSubmit = async () => {
+    try {
+      const response = await userService.register(this.state.data);
+      localStorage.setItem("token", response.headers["x-auth-token"]);
+      this.props.history.push("/");
+    } catch (ex) {
+      if (ex.response && ex.response.status === 400) {
+        console.log(ex);
+        const errors = { ...this.state.errors };
+        errors.email = ex.response.data;
+        this.setState({ errors });
+        console.log(errors);
+      }
+    }
+  };
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        {this.renderInput("fullName", "Full Name", "text", null, 6)}
-        {this.renderInput("username", "Username")}
+        {this.renderInput(
+          "email",
+          "E-mail Address",
+          "email",
+          "We won't share your e-mail with anyone",
+          6
+        )}
         {this.renderInput("password", "Password", "password")}
-        {this.renderButton("Submit")}
+        {this.renderInput("name", "Full Name")}
+        {this.renderButton("Register")}
       </form>
     );
   }
