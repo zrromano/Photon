4e887057a574c963a86c70c2e15700762e537451
zrromano <crazyscarecrow2@hotmail.com>
2019-11-14 20:17:23 -0800

    app template created

--- reference/react-app-template/src/App.js
+++ reference/react-app-template/src/App.js
@@ -1,13 +1,9 @@
 import React, { Component } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import Movies from "./components/movies";
-import Customers from "./components/customers";
-import Rentals from "./components/rentals";
 import NavBar from "./components/common/navbar";
 import NotFound from "./components/common/notFound";
 import Home from "./components/home";
-import MovieForm from "./components/movieForm";
 import Login from "./components/loginForm";
 import RegisterForm from "./components/registerForm";
 import Logout from "./components/common/logout";
@@ -26,11 +22,7 @@ class App extends Component {
 
   render() {
     const { user } = this.state;
-    const links = [
-      { path: "/movies", name: "Movies" },
-      { path: "/customers", name: "Customers" },
-      { path: "/rentals", name: "Rentals" }
-    ];
+    const links = [];
     return (
       <main className="container-fluid" role="main">
         <NavBar links={links} user={user} />
@@ -40,13 +32,6 @@ class App extends Component {
             <Route path="/register" component={RegisterForm} />
             <Route path="/login" component={Login} />
             <Route path="/logout" component={Logout} />
-            <ProtectedRoute path="/movies/:id" component={MovieForm} />
-            <Route
-              path="/movies"
-              render={props => <Movies {...props} user={user} />}
-            />
-            <Route path="/customers" component={Customers} />
-            <Route path="/rentals" component={Rentals} />
             <Route path="/not-found" component={NotFound} />
             <Route path="/" exact component={Home} />
             <Redirect to="/not-found" />
